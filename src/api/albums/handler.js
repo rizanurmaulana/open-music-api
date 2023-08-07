ce0237d609c68f8c987f1322d978228965b29fbf
rizanurmaulana <rizanurmaulana2810@gmail.com>
2023-08-07 10:46:34 +0700

    add getDetailAlbum

--- src/api/albums/handler.js
+++ src/api/albums/handler.js
@@ -31,14 +31,14 @@ class AlbumsHandler {
 
   async getAlbumByIdHandler(request) {
     const { id } = request.params;
-
     const album = await this._service.getAlbumById(id);
-
+    const songs = await this._service.getSongByAlbumId(id);
+    const getDetailAlbum = { ...album, songs };
     return {
       status: 'success',
-      message: 'Mendapatkan album berdasarkan id',
+      message: 'mendapatkan album berdasarkan id',
       data: {
-        album,
+        album: getDetailAlbum,
       },
     };
   }
